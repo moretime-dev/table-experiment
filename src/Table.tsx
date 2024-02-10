09fa46055412c82f924218c6d90883c85e495800
moretime-dev <moretimeproductions@googlemail.com>
2024-02-10 16:03:37 +0100

    inner table pagination works

--- src/Table.tsx
+++ src/Table.tsx
@@ -4,8 +4,20 @@ import {
   dynamicHeaderEntries,
   tableContentInformation,
 } from "./tableContent";
+import { useState } from "react";
 
 function TableComponent() {
+  const [index, setIndex] = useState(0);
+
+  const handleNextPage = () => {
+    if (index === dynamicHeaderEntries.length - 3) return setIndex(0);
+    setIndex((prev) => prev + 1);
+  };
+  const handlePrevPage = () => {
+    if (index === 0) return;
+    setIndex((prev) => prev - 1);
+  };
+
   return (
     <Table striped bordered hover style={{ width: "80vw", margin: "auto" }}>
       <thead>
@@ -13,29 +25,32 @@ function TableComponent() {
           {fixedHeaderEntries.map((fixedHeaderEntry) => (
             <td key={fixedHeaderEntry}>{fixedHeaderEntry}</td>
           ))}
-          <td>{dynamicHeaderEntries[0]}</td>
-          <td>{dynamicHeaderEntries[1]}</td>
-          <td>{dynamicHeaderEntries[2]}</td>
+          <td>{dynamicHeaderEntries[index]}</td>
+          <td>{dynamicHeaderEntries[index + 1]}</td>
+          <td>{dynamicHeaderEntries[index + 2]}</td>
           <td
             style={{
               display: "flex",
               justifyContent: "space-around",
             }}
           >
-            <button>prev</button>
-            <button>next</button>
+            <button onClick={handlePrevPage}>prev</button>
+            <button onClick={handleNextPage}>next</button>
           </td>
         </tr>
       </thead>
       <tbody>
         {tableContentInformation.map((info) => (
-          <tr>
+          <tr key={info.firstName}>
             <td>
               <input type="checkbox" /> {info.position}
             </td>
             <td>{info.firstName}</td>
             <td>{info.lastName}</td>
             <td>{info.userName}</td>
+            <td>{info.innerPagesInfo[index]}</td>
+            <td>{info.innerPagesInfo[index + 1]}</td>
+            <td>{info.innerPagesInfo[index + 2]}</td>
           </tr>
         ))}
       </tbody>
